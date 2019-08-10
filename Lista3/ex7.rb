b4075199726620acd7d2d3bf7c8a04d2ddf72242
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-08-10 20:38:23 -0300

    Delete ex7.rb

--- Lista3/ex7.rb
+++
@@ -1,9 +0,0 @@
-def symbolize_keys(hash)
-    array =  hash.keys.map { |x| x.to_sym}
-    hash[array[1]] = hash.delete("Segundo")
-    hash[array[0]] = hash.delete("Primeiro")
-    print hash
-end
-hash = {1 => 'Primeiro', 2 => 'Segundo'}
-symbolize_keys(hash)
-puts
